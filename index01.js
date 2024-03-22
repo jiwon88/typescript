ef8301a723eaa0f93bcc27bcbcca782f2ecbc234
jiwon88 <wldnjsdptmxp@gmail.com>
2024-03-22 16:42:04 +0900

    강의2

---
+++ index01.js
@@ -0,0 +1,10 @@
+var 네임 = 'kim';
+var 나이 = 50;
+var 결혼했니 = true;
+var 회원들 = ['kim', 'park'];
+var 회원들1 = { member1: 'kim', member2: 'park' };
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
---
+++ index01.ts
@@ -0,0 +1,17 @@
+let 네임 :string = 'kim';
+let 나이 :number = 50;
+let 결혼했니 :boolean = true;
+
+let 회원들 :string[] = ['kim', 'park']
+
+let 회원들1:{member1:string , member2:string} = { member1:'kim' , member2:'park'}
+
+let project :{
+    member: string[],
+    days: number,
+    started: boolean,
+}= {
+    member: ['kim', 'park'],
+    days: 30,
+    started : true,
+}
