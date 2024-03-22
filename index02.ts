6f300a8d357056acd5f95338fa4c2b42a495f851
jiwon88 <wldnjsdptmxp@gmail.com>
2024-03-22 18:15:34 +0900

    세번째

---
+++ index02.ts
@@ -0,0 +1,11 @@
+let a :number | string = 123;
+a = 123;
+
+let 회원 :(number | string)[] = [1,'2',3];
+let 오브젝트 : {a: string | number} = { a : 123}
+
+let 이름12 :unknown;
+이름12 = 123;
+이름12 = [];
+
+let 나이12 :unknown = 1;
